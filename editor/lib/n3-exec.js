8a208ab2c35e21afcf7baf6408d53f3e942ad251
darth_will <william.van.woensel@gmail.com>
2020-06-03 11:16:28 -0300

    url-encoding hashes so they're not delimiters

--- editor/lib/n3-exec.js
+++ editor/lib/n3-exec.js
@@ -1,6 +1,6 @@
 n3.exec = function(url, options, onSuccess, onError) {
 	$.post(url, options, (output, status) => {
-			console.log(status, output)
+			// console.log(status, output)
 			
 			if (output.success !== undefined)
 				onSuccess(output.success)
