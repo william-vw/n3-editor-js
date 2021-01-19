51296f8931e5a2ad6628ea26179ec6925cd769ec
darth_will <william.van.woensel@gmail.com>
2021-01-19 15:12:29 -0400

    fixed merge conflict

--- config.js
+++ config.js
@@ -1,18 +1,21 @@
 exports.http = {
-	hostname: '127.0.0.1',
-	// hostname: 'ppr.cs.dal.ca',
+	// hostname: '127.0.0.1',
+	hostname: 'ppr.cs.dal.ca',
 	port: 3002
 }
 
 exports.exec = {
 	eyePath: "/opt/eye/bin/eye.sh",
-	pythonCmd: "C:/Python27/python.exe",
-	// cwmPath: "/opt/cwm-1.2.1/swap/cwm.py",
-	cwmPath: "C:/cwm/cwm-1.2.1/swap/cwm.py"
+	
+	pythonCmd: "python", // use python2 for cwm
+	// pythonCmd: "C:/Python27/python.exe", // use python2 for cwm
+	
+	cwmPath: "/opt/cwm-1.2.1/swap/cwm.py",
+	// cwmPath: "C:/cwm/cwm-1.2.1/swap/cwm.py"
 }
 
 exports.db = {
 	user: 'root',
-	pwd: '12345',
-	// pwd: 'changeit'
+	// pwd: '12345',
+	pwd: 'changeit'
 }
