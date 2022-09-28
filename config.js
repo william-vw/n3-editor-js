b8f28a9b5af5d0907287a6588ffd5757006fdab5
William Van Woensel <wvw@Williams-MBP.lan>
2022-09-28 09:25:53 -0400

    Revert "adding mysql"
    This reverts commit 10186223c47d3e511b80c47c4db2c3dea8f1cc9b.

--- config.js
+++ config.js
@@ -9,48 +9,33 @@ exports.http = {
 
 exports.reasoners = {
 	eye: {
-		// - local, ppr
 		// exec: "/opt/eye/bin/eye.sh", // ppr
 		// folder: "/Users/wvw/git/n3/n3-editor-js/lib/eye" // local
-		// - heroku
-		exec: "/app/opt/eye/bin/eye", 
-		folder: "/app/lib/eye"
+		exec: "/app/opt/eye/bin/eye", // heroku
+		folder: "/app/lib/eye" // heroku
 	},
 	cwm: {
-		// - local, ppr
 		// (use python2 for cwm)
 		pythonCmd: "python", // local, ppr
 		// exec: "/opt/cwm-1.2.1/swap/cwm.py" // ppr
 		// exec: "/Users/wvw/cwm-1.2.1/swap/cwm.py" // local
-		// - heroku
-		pythonCmd: "", // (python2 is not available)
-		exec: ""
+		
+		pythonCmd: "", // heroku (python2 is not available)
+		exec: "" // heroku
 	},
 	jen3: {
-		// - local
-		// exec: "/Users/wvw/git/n3/n3-editor-js/lib/jen3/jen3.jar",
-		// folder: "/Users/wvw/git/n3/n3-editor-js/lib/jen3",
-		// - heroku
-		exec: "/app/lib/jen3/jen3.jar",
-		folder: "/app/lib/jen3"
+		// exec: "/Users/wvw/git/n3/n3-editor-js/lib/jen3/jen3.jar", // local
+		// folder: "/Users/wvw/git/n3/n3-editor-js/lib/jen3", // local
+		exec: "/app/lib/jen3/jen3.jar", // heroku
+		folder: "/app/lib/jen3" // heroku
 	}
 }
 
 exports.db = {
-	port: '33060',
-	// - local, ppr
-	// host: 'localhost',
-	// db: "n3_links",
-	// user: 'root', // local, ppr
-	// pwd: '12345', // (?)
+	user: 'root',
+	// pwd: '12345',
 	// pwd: 'changeit' // ppr (?)
-	// pwd: '' // local
-	// - heroku
-	// mysql://b4837d17c012f1:e023e78d@us-cdbr-east-06.cleardb.net/heroku_e750abd160bbcaf?reconnect=true
-	host: "us-cdbr-east-06.cleardb.net",
-	db: "heroku_e750abd160bbcaf",
-	user: "b4837d17c012f1",
-	pwd: "e023e78d"
+	pwd: '' // local
 }
 
 // exports.path = "/home/woensel/projects/n3-editor-js" // ppr
