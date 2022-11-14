8401d2a1fa21d98eeef374934650c21104ec439e
William Van Woensel <wvw@Williams-MBP.lan>
2022-11-14 13:11:20 -0500

    trying out eyebrow

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/n3/editor', express.static(path.join(__dirname, 'editor')));
+app.use('/n3/lib/eyebrow', express.static(path.join(__dirname, 'lib/eyebrow')));
 
 app.get('/n3/editor/s*', (request, response) => {
 	console.log("link:", request.url)
