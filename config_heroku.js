ad4acf556af6c808aea5bebec7a956dcd70507b5
William Van Woensel <wvw@df0f9.vpn.dal.ca>
2022-12-11 14:15:26 -0500

    heroku config

---
+++ config_heroku.js
@@ -0,0 +1,42 @@
+config = {
+	http: {
+		hostname: 'https://n3-editor.herokuapp.com',
+	    port: process.env.PORT
+	},
+
+	reasoners: {
+		eye: {
+			exec: "/app/opt/eye/bin/eye", 
+		    folder: "/app/lib/eye"
+		},
+		cwm: {
+			// (use python2 for cwm)
+			pythonCmd: "", // (python2 is not available)
+            exec: ""
+		},
+		jen3: {
+            exec: "/app/lib/jen3/jen3.jar",
+            codegen: "/app/lib/jen3/codegen.jar",
+		    folder: "/app/lib/jen3"
+		}
+	},
+
+	link: {
+		max_len: 50000,
+		db: {
+            // mysql://b4837d17c012f1:e023e78d@us-cdbr-east-06.cleardb.net/heroku_e750abd160bbcaf?reconnect=true
+			port: '33060',
+            host: "us-cdbr-east-06.cleardb.net",
+            db: "heroku_e750abd160bbcaf",
+            user: "b4837d17c012f1",
+            pwd: "e023e78d"
+		}
+	},
+
+	path: "/Users/wvw/git/n3/n3-editor-js" // ??
+}
+
+if (typeof exports === 'object' && typeof module === 'object')
+	module.exports = {
+		config
+	};
