441aa2ac878df956e23362c659e5377c06ec12cd
William Van Woensel <wvw@df0f9.vpn.dal.ca>
2022-12-11 14:27:24 -0500

    updated config

--- config.js
+++ config.js
@@ -1,38 +1,39 @@
 config = {
 	http: {
-		hostname: 'http://127.0.0.1',
-		port: 3002
+		hostname: 'https://n3-editor.herokuapp.com',
+	    port: process.env.PORT
 	},
 
 	reasoners: {
 		eye: {
-			exec: "/Users/wvw/git/n3/n3-editor-js/opt/eye/bin/eye",
-			folder: "/Users/wvw/git/n3/n3-editor-js/lib/eye"
+			exec: "/app/opt/eye/bin/eye", 
+		    folder: "/app/lib/eye"
 		},
 		cwm: {
 			// (use python2 for cwm)
-			pythonCmd: "python",
-			exec: "/Users/wvw/cwm-1.2.1/swap/cwm.py"
+			pythonCmd: "", // (python2 is not available)
+            exec: ""
 		},
 		jen3: {
-			exec: "/Users/wvw/git/n3/n3-editor-js/lib/jen3/jen3.jar",
-			codegen: "/Users/wvw/git/n3/n3-editor-js/lib/jen3/codegen.jar",
-			folder: "/Users/wvw/git/n3/n3-editor-js/lib/jen3"
+            exec: "/app/lib/jen3/jen3.jar",
+            codegen: "/app/lib/jen3/codegen.jar",
+		    folder: "/app/lib/jen3"
 		}
 	},
 
 	link: {
 		max_len: 50000,
 		db: {
+            // mysql://b4837d17c012f1:e023e78d@us-cdbr-east-06.cleardb.net/heroku_e750abd160bbcaf?reconnect=true
 			port: '33060',
-			host: 'localhost',
-			db: "n3_links",
-			user: 'root',
-			pwd: ''
+            host: "us-cdbr-east-06.cleardb.net",
+            db: "heroku_e750abd160bbcaf",
+            user: "b4837d17c012f1",
+            pwd: "e023e78d"
 		}
 	},
 
-	path: "/Users/wvw/git/n3/n3-editor-js"
+	path: "/Users/wvw/git/n3/n3-editor-js" // ??
 }
 
 if (typeof exports === 'object' && typeof module === 'object')
