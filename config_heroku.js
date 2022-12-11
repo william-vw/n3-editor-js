8ffdb2fa0298f601290fe1640e0268fa87618ee3
William Van Woensel <wvw@df0f9.vpn.dal.ca>
2022-12-11 15:28:45 -0500

    fix gen_link

--- config_heroku.js
+++ config_heroku.js
@@ -1,7 +1,7 @@
 config = {
 	http: {
 		hostname: 'https://n3-editor.herokuapp.com',
-	    port: process.env.PORT
+	    port: ((typeof process) != "undefined" ? process.env.PORT : undefined)
 	},
 
 	reasoners: {
