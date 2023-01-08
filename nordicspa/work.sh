a1e7e4cf7b408360dd9c2aa251d5a4cc9df45d83
William Van Woensel <wvw@Williams-MBP.lan>
2023-01-08 09:47:06 -0500

    add crawler

--- nordicspa/work.sh
+++ nordicspa/work.sh
@@ -1,3 +1,3 @@
-java -jar nordicspa.jar -days fri,sat,sun -months 2023-01,2023-02,2023-03 -num 2 -recip william.van.woensel@gmail.com,kelseyatjones@gmail.com
-java -jar nordicspa.jar -days fri,sat,sun -months 2023-01,2023-02,2023-03 -num 3 -recip william.van.woensel@gmail.com,kelseyatjones@gmail.com
-java -jar nordicspa.jar -days fri,sat,sun -months 2023-01,2023-02,2023-03 -num 4 -recip william.van.woensel@gmail.com,kelseyatjones@gmail.com
+java -jar nordicspa/nordicspa.jar -days fri,sat,sun -months 2023-01,2023-02,2023-03 -num 2 -recip william.van.woensel@gmail.com,kelseyatjones@gmail.com
+java -jar nordicspa/nordicspa.jar -days fri,sat,sun -months 2023-01,2023-02,2023-03 -num 3 -recip william.van.woensel@gmail.com,kelseyatjones@gmail.com
+java -jar nordicspa/nordicspa.jar -days fri,sat,sun -months 2023-01,2023-02,2023-03 -num 4 -recip william.van.woensel@gmail.com,kelseyatjones@gmail.com
