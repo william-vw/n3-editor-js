567dc9ce05891d2dd71de5156e693c887b84807f
William Van Woensel <wvw@Williams-MacBook-Pro.local>
2023-01-10 13:34:18 -0500

    updated config for use with editor

--- editor/webpack.config.js
+++ editor/webpack.config.js
@@ -4,25 +4,6 @@ const path = require('path');
 
 // -- script
 // (for use by n3-editor-js)
-// module.exports = {
-//   // exclude node.js-only modules
-//   // node: { module: "empty", net: "empty", fs: "empty" }, // older versions of webpack
-//   resolve: { fallback: { fs: false } }, // webpack v5
-
-//   entry: './parser/n3/index.js',
-//   output: {
-//     filename: 'n3Main.js',
-//     libraryTarget: 'var',
-//     library: 'n3',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   optimization: {
-//     minimize: true
-//   }
-// };
-
-// -- requires
-// (for use by VSCode extension)
 module.exports = {
   // exclude node.js-only modules
   // node: { module: "empty", net: "empty", fs: "empty" }, // older versions of webpack
@@ -30,18 +11,37 @@ module.exports = {
 
   entry: './parser/n3/index.js',
   output: {
-    filename: 'n3Main_umd.js',
+    filename: 'n3Main.js',
+    libraryTarget: 'var',
+    library: 'n3',
     path: path.resolve(__dirname, 'dist'),
-    globalObject: 'this',
-    library: {
-      name: 'n3Main',
-      type: 'umd',
-    },
   },
   optimization: {
-    minimize: false
+    minimize: true
   }
 };
+
+// -- requires
+// (for use by VSCode extension)
+// module.exports = {
+//   // exclude node.js-only modules
+//   // node: { module: "empty", net: "empty", fs: "empty" }, // older versions of webpack
+//   resolve: { fallback: { fs: false } }, // webpack v5
+
+//   entry: './parser/n3/index.js',
+//   output: {
+//     filename: 'n3Main_umd.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     globalObject: 'this',
+//     library: {
+//       name: 'n3Main',
+//       type: 'umd',
+//     },
+//   },
+//   optimization: {
+//     minimize: false
+//   }
+// };
 
 // - TurtleStar
 
