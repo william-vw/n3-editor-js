25b84bcc9dbec126fe819b69c7e5aeb1dc287954
William Van Woensel <wvw@Williams-MacBook-Pro.local>
2023-01-10 12:30:47 -0500

    minify parsers & lexers

--- editor/webpack.config.js
+++ editor/webpack.config.js
@@ -8,11 +8,11 @@ module.exports = {
   // node: { module: "empty", net: "empty", fs: "empty" }, // older versions of webpack
   resolve: { fallback: { fs: false } }, // webpack v5
 
-  entry: './parser/turtlestar/index.js',
+  entry: './parser/n3/index.js',
   output: {
-    filename: 'turtlestarMain.js',
+    filename: 'n3Main.js',
     libraryTarget: 'var',
-    library: 'turtlestar',
+    library: 'n3',
     path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
