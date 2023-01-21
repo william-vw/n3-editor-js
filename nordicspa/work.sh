73049ab0da522861a8ed3b2ec7b3d037947aee9f
William Van Woensel <wvw@Williams-MBP.lan>
2023-01-21 09:35:34 -0500

    updating work.sh

--- nordicspa/work.sh
+++ nordicspa/work.sh
@@ -1,3 +1,1 @@
-java -jar nordicspa/nordicspa.jar -days sat,sun -months 2023-01,2023-02,2023-03 -num 2 -recip william.van.woensel@gmail.com,kelseyatjones@gmail.com
-java -jar nordicspa/nordicspa.jar -days sat,sun -months 2023-01,2023-02,2023-03 -num 3 -recip william.van.woensel@gmail.com,kelseyatjones@gmail.com
 java -jar nordicspa/nordicspa.jar -days sat,sun -months 2023-01,2023-02,2023-03 -num 4 -recip william.van.woensel@gmail.com,kelseyatjones@gmail.com
